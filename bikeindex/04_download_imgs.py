c1c939c1c5cafb8a50f0060cef00fc437a5abf86
Thorben Jensen <jensen.thorben@gmail.com>
2019-10-05 07:04:15 +0200

    adding DVC stage 'download iamges"

--- bikeindex/04_download_imgs.py
+++ bikeindex/04_download_imgs.py
@@ -8,8 +8,7 @@ import pandas as pd
 IMG_DIR = "data/download/"
 
 # %% LOAD DATA
-df_merged = pd.read_csv("data/df_merged.csv", delimiter=";", quotechar="'") \
-    .assign()
+df_merged = pd.read_csv("data/df_merged.csv", delimiter=";", quotechar="'").assign()
 
 
 # %% IMAGE ID
@@ -24,11 +23,19 @@ assert not df_merged.thumbnail_id.duplicated().any(), "Duplicated thumbnail ids!
 if not os.path.exists(IMG_DIR):
     os.makedirs(IMG_DIR)
 
+print("Downloading new files...")
 for link, id in zip(df_merged.thumbnail, df_merged.thumbnail_id):
-    print(f"downloading image {id}.jpg "
-          f"from {link} "
-          f"({(df_merged.thumbnail_id==id).idxmax() + 1}/{len(df_merged)})...")
-    urllib.request.urlretrieve(link, IMG_DIR + id + ".jpg")
-    time.sleep(2)
+    outfile = IMG_DIR + id + ".jpg"
+    if not os.path.exists(outfile):
+        print(
+            f"downloading image {id}.jpg "
+            f"from {link} "
+            f"({(df_merged.thumbnail_id==id).idxmax() + 1}/{len(df_merged)})..."
+        )
+        try:
+            urllib.request.urlretrieve(link, outfile)
+        except urllib.error.HTTPError:
+            continue
+        time.sleep(2)
 
 # %%
